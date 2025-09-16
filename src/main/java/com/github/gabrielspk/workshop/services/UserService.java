46d52d76a72e6e7f58532512abe17d39985f109a
gabrielspk <gabrielsilvaferreira007@gmail.com>
2025-09-16 15:13:19 -0300

    feat: add user find by id

--- src/main/java/com/github/gabrielspk/workshop/services/UserService.java
+++ src/main/java/com/github/gabrielspk/workshop/services/UserService.java
@@ -1,20 +1,27 @@
 package com.github.gabrielspk.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.gabrielspk.workshop.domain.User;
 import com.github.gabrielspk.workshop.repositories.UserRepository;
+import com.github.gabrielspk.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 	
 	public List<User> findAll() {
-		return repository.findAll();
+		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
