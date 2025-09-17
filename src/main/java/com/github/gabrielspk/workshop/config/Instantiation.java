66ae8ed14532628822b5602fd3a9bcbf6bd028d5
gabrielspk <gabrielsilvaferreira007@gmail.com>
2025-09-17 15:06:40 -0300

    feat: create Post entity with nested User reference

--- src/main/java/com/github/gabrielspk/workshop/config/Instantiation.java
+++ src/main/java/com/github/gabrielspk/workshop/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.github.gabrielspk.workshop.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.gabrielspk.workshop.domain.Post;
 import com.github.gabrielspk.workshop.domain.User;
+import com.github.gabrielspk.workshop.repositories.PostRepository;
 import com.github.gabrielspk.workshop.repositories.UserRepository;
 
 @Configuration
@@ -14,16 +18,29 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		LocalDate d1 = LocalDate.parse("21/03/2025", formatter);
+		Post post1 = new Post(null, d1, "Partiu viagem", "Vou viajar pra São Paulo", maria);
+		
+		LocalDate d2 = LocalDate.parse("23/03/2025", formatter);
+		Post post2 = new Post(null, d2, "Bom dia", "Acordei feliz hoje", bob);
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 }
---
+++ src/main/java/com/github/gabrielspk/workshop/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.github.gabrielspk.workshop.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.gabrielspk.workshop.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
